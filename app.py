0597d8b9895a9e299afbce95e4db6b320d4422d2
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2024-12-07 12:43:40 +0200

    feat: add middleware for error handling to app

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify, render_template
 from controllers.translation_controller import translate_text
+from middlewares.error_handler import handle_errors
 
 app = Flask(__name__)
 
+# Register middleware
+app.register_error_handler(Exception, handle_errors)
 
 # @app.route("/1")
 # def home1():
