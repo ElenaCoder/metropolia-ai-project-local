2d773cb35dc72b5c1d921f28b7af9cd8bc86b53d
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2024-12-07 12:38:21 +0200

    feat: add new /translate route handling

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify, render_template
+from controllers.translation_controller import translate_text
 
 app = Flask(__name__)
+
+
+# @app.route("/1")
+# def home1():
+#     return jsonify({"message": "Hello, Flask!"})
 
 # Routes
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/translate", methods=["POST"])
+def translate():
+    return translate_text(request)
 
 
 if __name__ == "__main__":
